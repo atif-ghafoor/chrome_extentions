4515f1cd5368e73e19b650dc57d957a0ff5372dc
atif-ghafoor <atifghafoor377@gmail.com>
2024-12-27 17:15:58 +0500

    code background script for extention button click to load function

--- practise extention/background.js
+++ practise extention/background.js
@@ -1,1 +1,9 @@
 console.log("background is running");
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+let clicked = false;
+
+function buttonClicked(tabs) {
+  clicked = !clicked;
+  chrome.tabs.sendMessage(tabs.id, clicked);
+}
