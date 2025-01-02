6bded3bce1e54554e78c538a4df7634d79644161
atif-ghafoor <atifghafoor377@gmail.com>
2025-01-02 19:32:17 +0500

    add 5 character lenth for check of selcted text

--- Translate/froented/content.js
+++ Translate/froented/content.js
@@ -2,7 +2,7 @@ window.addEventListener("mouseup", getSelctedWord);
 
 function getSelctedWord() {
   const selctedText = window.getSelection().toString();
-  if (selctedText.length > 0) {
+  if (selctedText.length > 5) {
     console.log(selctedText, selctedText.length);
     chrome.runtime.sendMessage({ action: "text", message: selctedText });
   }
